e8881d8bf9534e36d9c0baac323f055941f36673
Roh Seung Chan <70617665+rch1025@users.noreply.github.com>
2022-10-31 21:27:42 +0900

    [level 2] Title: 소수 찾기, Time: 0.91 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -7,13 +7,13 @@ def solution(numbers):
     
     ww = list(set(ww))
     for w_one in ww:
-        if (w_one==1) or (w_one==0): # 0혹은 1이면 취소
+        if (w_one==1) or (w_one==0): # 0혹은 1이면 skip -> 소수에 해당 안 됨
             continue
         count = 0
-        for idx in range(2, int(w_one**0.5)+1):
+        for idx in range(2, int(w_one**0.5)+1): # 루트의 +1한 것만 비교하기
             if w_one % idx == 0:
-                count += 1
+                count += 1 # 1 혹은 자기 자신이 아닌 다른 수에서 약분이 되면 count에 1 추가
         if count==0:
-            answer += 1
+            answer += 1 # count가 0이라면 소수이기에 +1
             
     return answer
